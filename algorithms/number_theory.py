c7fba46a0a5965cd0fff9403fe34ce8d848676a5
Meghanadh <jannimarrimeghanadh@gmail.com>
2026-02-13 04:49:20 +0530

    Add sieve of Eratosthenes

--- algorithms/number_theory.py
+++ algorithms/number_theory.py
@@ -11,3 +11,18 @@ def is_prime(n):
             return False
     return True
 
+def sieve_of_eratosthenes(n):
+    if n < 2:
+        return []
+
+    is_prime = [True] * (n + 1)
+    is_prime[0] = is_prime[1] = False
+
+    for i in range(2, int(n ** 0.5) + 1):
+        if is_prime[i]:
+            for j in range(i * i, n + 1, i):
+                is_prime[j] = False
+
+    return [i for i in range(2, n + 1) if is_prime[i]]
+
+
