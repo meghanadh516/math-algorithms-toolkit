4fcb30f59a7cd122860a2d19bbfbcb8176778a7c
Meghanadh <jannimarrimeghanadh@gmail.com>
2026-02-16 04:25:57 +0530

    Configured setup.py for package installation

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="math-algorithms-toolkit",
+    version="0.1.0",
+    description="A collection of math algorithms including algebra, matrix, graph, and calculus.",
+    author="Meghanadh",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
